27cc746264f8938e111f78e5f0ca9111ec11217c
Luis D. Lafaurie <luis.lafaurie@tecnativa.com>
2022-11-12 11:16:31 +0100

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-  'name': "Open Academy",
-  'summary': """Managing trainings""",
-  'description': """
-    
-  
-  """
+    'name': "Open Academy",
+    'summary': """Managing trainings""",
+    'description': """
+        Open Academy module for managing trainings:
+            #. Training courses
+            #. Training sessions
+            #. Attendance registration
+    """,
+    'author': "Lvis",
+    'website': "https://www.lvis.com",
+    'category': 'Test',
+    'version': '14.0.1.0.0',
+    'depends': ['base'],
+    'data': [
+        'templates.xml',    
+    ],
+    'demo': [
+        'demo.xml',
+    ],
 }
